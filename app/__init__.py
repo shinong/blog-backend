12423f53731139b19ccfc16e6ad73083929a31f2
Shinong <shinongmao@gmail.com>
2024-02-08 09:15:34 +0800

    setup db crud

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
+
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'app.sqlite')
+)
+
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+from . import db
+db.init_app(app)
 
 from app import api
--- app/api.py
+++ app/api.py
@@ -1,6 +1,9 @@
 # This file contains most of logis and features
 import platform
+import uuid
+from flask import request, jsonify
 from app import app
+from app.db import get_db
 
 ## devlopment tools: /dev/
 
@@ -11,3 +14,50 @@ def dev_env():
         "backend":"Flask",
         "os": platform.platform()
     }
+
+## prod routes:
+
+# this is the endpoint for open form registration
+@app.route("/prod/openregistration",methods=["post"])
+def open_registration():
+    content = request.json
+    first_name = content["firstName"]
+    last_name = content["lastName"]
+    phone = content["phone"]
+    email = content["email"]
+    status = content['status']
+    department = content["department"]
+    username = str(uuid.uuid4())
+    password = ""
+    db = get_db()
+    error = None
+    if not first_name:
+        error = "first name not defined"
+    elif not status:
+        error = "student status not defined"
+    if error is None:
+        try:
+            db.execute(
+                "INSERT INTO user (username, password, first_name, last_name, phone, email, status, department) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                (username, password, first_name, last_name, phone, email, status, department),
+            )
+            db.commit()
+            return "OK"
+        except db.IntegrityError:
+            error = f"User {username} is already registered."
+    return error
+
+# this is the route to fetch the registered users from db
+@app.route("/prod/fetchallusers",methods=["get"])
+def fetch_all_users():
+    db = get_db()
+    try:
+        cur = db.cursor()
+        cur.execute('SELECT * FROM user')
+        res = [dict((cur.description[i][0], value) \
+               for i, value in enumerate(row)) for row in cur.fetchall()]
+        db.commit()
+    except db.IntegrityError:
+        error = f"db error"
+        return error
+    return jsonify(res)
