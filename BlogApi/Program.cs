36b9af358926817417535b1fc16271cbbbf1aed8
Shinong <shinongmao@gmail.com>
2024-10-15 17:00:13 -0400

    update to dotnet8

--- BlogApi/Program.cs
+++ BlogApi/Program.cs
@@ -9,7 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoContext>(
+    opt => opt.UseSqlServer("TodoList")
+    );
 
 var app = builder.Build();
 
